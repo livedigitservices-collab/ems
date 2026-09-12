b5b44ef5368fa36e2520a62aa60615d8202c8b6b
livedigitservices <livedigitservices2@gmail.com>
2026-09-12 12:06:38 +0530

    resloved cors error

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 const allowedOrigins = [
-    "https://livedigit-ems.vercel.app",
+    "https://ems-eight-mauve.vercel.app/",
     "http://localhost:5173"
 ]
 
